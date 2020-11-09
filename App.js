0e8adb5c6fb198ddcdb8fe67232b252a2307009d
OlukaDenis <olukadeno@gmail.com>
2020-11-10 01:23:34 +0300

    Add redux store

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Home from './src/Home';
+import store from './src/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>  Tour app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store} >
+      <SafeAreaView style={styles.container}>
+        <Home />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+
+const Home = () => {
+
+  const createTwoButtonAlert = () =>
+    Alert.alert(
+      "Alert Title",
+      "My Alert Msg",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ],
+      { cancelable: false }
+    );
+
+  return (
+    <View style={styles.container}>
+      <Text>
+        Let's tour
+      </Text>
+      <TextInput
+        placeholder="Enter name"
+        />
+      <Button
+      onPress={createTwoButtonAlert}
+        title="Enter"
+        color="#0064e1"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#F5FCFF',
+  },
+})
+export default Home
