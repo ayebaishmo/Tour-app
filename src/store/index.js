6c112177004375f40ff009d9b14709d75b7ebcbc
OlukaDenis <olukadeno@gmail.com>
2021-05-23 20:18:46 +0300

    :changed: API keys

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import reducer from './reducers/index';
 
